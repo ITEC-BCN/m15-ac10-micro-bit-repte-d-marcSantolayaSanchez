a1d91fb9f8cd19fdd2841bbf4d10f878a9ceeee5
marcSantolayaSanchez <146827090+marcSantolayaSanchez@users.noreply.github.com>
2024-10-28 15:49:17 +0100

    Apartado 4 y 5 hechos

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+let hola = ""
 basic.forever(function () {
-	
+    if (input.temperature() > 22) {
+        hola = "Calor"
+        basic.showString("Calor")
+    } else {
+        hola = "Frio"
+        basic.showString("Frio")
+    }
+})
+basic.forever(function () {
+    if (input.lightLevel() > 200) {
+        music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.UntilDone)
+    } else if (input.lightLevel() < 50) {
+        music.play(music.stringPlayable("E B C5 A B G A F ", 120), music.PlaybackMode.UntilDone)
+    } else {
+    	
+    }
 })
